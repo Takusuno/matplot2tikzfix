a5a0276a0402757ab3af4eb55aa5212acb1f134b
Erwin de Gelder <erwin.degelder@tno.nl>
2025-04-23 23:36:32 +0200

    Run test_legend3 test. Provide alternative reference for Python 3.9 and above.

--- tests/test_legend3.py
+++ tests/test_legend3.py
@@ -1,8 +1,15 @@
+"""Test legend with 3 entries."""
+
+import matplotlib as mpl
 import matplotlib.pyplot as plt
+from matplotlib.figure import Figure
+
+from .helpers import assert_equality
+
+mpl.use("Agg")
 
 
-def plot():
-    # mpl.style.use("seaborn-colorblind")
+def plot() -> Figure:
     fig = plt.figure(figsize=(7, 4))
     ax = fig.add_subplot(111)
     ax.fill_between([1, 2], [2, 2], [3, 3], color="red", alpha=0.2, label="roh")
@@ -11,3 +18,12 @@ def plot():
     ax.grid()
     plt.legend()
     return fig
+
+
+def test() -> None:
+    try:
+        assert_equality(plot, __file__[:-3] + "_reference.tex")
+    except AssertionError:
+        # Try other output, which is the new output since Python 3.9.
+        # With Python 3.9 and above, legend will be at center right (which makes more sense).
+        assert_equality(plot, __file__[:-3] + "_reference2.tex")
